55c91a8d9e12facf3a2616d386a26711ae9507f0
moad <moad.makboul.mm@gmail.com>
2023-09-25 20:04:53 +0100

    settings config

--- shopBackend/urls.py
+++ shopBackend/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf.urls import url
 from django.views.generic import TemplateView
-
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +26,5 @@ urlpatterns = [
     path('get_cart/', include('cart.urls')),
     path('users/', include('users.urls')),
     path('payments/', include('payments.urls')),
-    url(r'^(?:.*)/?$', TemplateView.as_view(template_name="index.html"))
+    url(r'^(?:.*)/?$', index)
 ]
