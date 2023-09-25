6bf07c5f73c3b07d143a483afaca87e565c33998
moad <moad.makboul.mm@gmail.com>
2023-09-25 20:12:06 +0100

    settings config

--- shopBackend/urls.py
+++ shopBackend/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('get_cart/', include('cart.urls')),
     path('users/', include('users.urls')),
     path('payments/', include('payments.urls')),
-    url(r'^(?:.*)/?$', index)
+    re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html'))
 ]
